73b162267bbfab7137a0557511ceede754f0b18b
Taihoon Kim <kishu@navercorp.com>
2018-06-01 14:56:36 +0900

    refactpr: remove video list item component

--- src/app/components/video-list/video-list.component.ts
+++ src/app/components/video-list/video-list.component.ts
@@ -1,14 +1,14 @@
 import { PLATFORM_ID, Component, Injectable, Inject, OnInit } from '@angular/core';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { AngularFireAction, DatabaseSnapshot } from 'angularfire2/database';
+import { SafeStyle, DomSanitizer } from '@angular/platform-browser';
 import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { flatMap, map, tap, toArray, scan, switchMap} from 'rxjs/operators';
+import { AngularFireAction, DatabaseSnapshot } from 'angularfire2/database';
 
 import { IVideo } from '../../interfaces/video';
 import { VideosService } from '../../services/videos/videos.service';
 
-@Injectable()
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
@@ -27,6 +27,7 @@ export class VideoListComponent implements OnInit {
     @Inject('LOCALSTORAGE') private localStorage: any,
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private sanitizer: DomSanitizer,
     private videosService: VideosService) {}
 
   ngOnInit() {
@@ -37,6 +38,11 @@ export class VideoListComponent implements OnInit {
         scan((acc, curr) => acc.concat(curr.reverse()), []),
         tap(_ => this.loading = false)
       );
+  }
+
+  private background(video) {
+    return this.sanitizer.
+      bypassSecurityTrustStyle(`url(https://img.youtube.com/vi/${video.youtube_id}/0.jpg) center / cover`);
   }
 
   private onMore() {
--- src/app/modules/videos/videos.module.ts
+++ src/app/modules/videos/videos.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { VideosRoutingModule } from './videos-routing.module';
 import { VideosService } from '../../services/videos/videos.service'
 import { VideoListComponent } from '../../components/video-list/video-list.component';
-import { VideoListItemComponent } from '../../components/video-list-item/video-list-item.component';
 import { VideoDetailComponent } from '../../components/video-detail/video-detail.component';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { VideoDetailComponent } from '../../components/video-detail/video-detail
   ],
   declarations: [
     VideoListComponent,
-    VideoListItemComponent,
     VideoDetailComponent
   ],
   providers: [VideosService]
